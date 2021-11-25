8a7301cddd48a2f89c3ce8e358f1273f78de81ed
David <d6797125@pm.me>
2021-11-25 19:01:19 +0100

    feat: update RecentLocationDto and add attributes

--- TourGuide/src/main/java/tourGuide/dto/RecentLocationDto.java
+++ TourGuide/src/main/java/tourGuide/dto/RecentLocationDto.java
@@ -1,4 +1,12 @@
 package tourGuide.dto;
 
+import gpsUtil.location.Location;
+
+import java.util.List;
+import java.util.UUID;
+
 public class RecentLocationDto {
+
+    public UUID userId;
+    public List<Location> location;
 }
