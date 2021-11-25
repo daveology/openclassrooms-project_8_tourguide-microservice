f39623b8bbb35f780e70f07d1eb82e92295aaac2
David <d6797125@pm.me>
2021-11-25 19:01:35 +0100

    feat: update RecentLocationDto and add getters/setters

--- TourGuide/src/main/java/tourGuide/dto/RecentLocationDto.java
+++ TourGuide/src/main/java/tourGuide/dto/RecentLocationDto.java
@@ -9,4 +9,20 @@ public class RecentLocationDto {
 
     public UUID userId;
     public List<Location> location;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public List<Location> getLocation() {
+        return location;
+    }
+
+    public void setLocation(List<Location> location) {
+        this.location = location;
+    }
 }
