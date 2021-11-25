08a7eb7730479176e66cd937fe79e8a0cad60b11
David <d6797125@pm.me>
2021-11-25 21:31:34 +0100

    fix: add absolute value to getUsersRecentLocations condition (unit test passed)

--- TourGuide/src/main/java/tourGuide/service/TourGuideService.java
+++ TourGuide/src/main/java/tourGuide/service/TourGuideService.java
@@ -196,7 +196,8 @@ public class TourGuideService {
 
 			u.getVisitedLocations().stream().forEach(v -> {
 				//logger.debug("Test: " + LocalDateTime.now().toInstant(ZoneOffset.UTC) + " " + v.timeVisited.toInstant());
-				if (LocalDate.now().until(v.timeVisited.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), ChronoUnit.DAYS) < withinDays) {
+				//logger.debug("Test: " + LocalDate.now().until(v.timeVisited.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), ChronoUnit.DAYS) + " days");
+				if (Math.abs(LocalDate.now().until(v.timeVisited.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), ChronoUnit.DAYS)) < withinDays) {
 					locations.add(v.location);
 				}
 			});
