2725db28556272bfac9a8e7007beda6137056307
David <d6797125@pm.me>
2021-12-14 14:42:21 +0100

    feat: add FeignClient to GpsUtilProxy

--- TourGuide/src/main/java/tourGuide/proxy/GpsUtilProxy.java
+++ TourGuide/src/main/java/tourGuide/proxy/GpsUtilProxy.java
@@ -1,5 +1,8 @@
 package tourGuide.proxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "GpsMicroservice", url = ":localhost:7911")
 public class GpsUtilProxy {
 
 
