97e662576465ceb852f820c3a66bacb547940208
David <d6797125@pm.me>
2021-12-14 14:44:42 +0100

    feat: add attractions proxy endpoint

--- TourGuide/src/main/java/tourGuide/proxy/GpsUtilProxy.java
+++ TourGuide/src/main/java/tourGuide/proxy/GpsUtilProxy.java
@@ -1,9 +1,14 @@
 package tourGuide.proxy;
 
+import gpsUtil.location.Attraction;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(name = "GpsMicroservice", url = ":localhost:7911")
-public class GpsUtilProxy {
+public interface GpsUtilProxy {
 
-
+    @GetMapping(value="/attractions")
+    List<Attraction> getAttractions();
 }
