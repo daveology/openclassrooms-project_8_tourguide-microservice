800d0fd5af4847c2de85bf0f826e3d90320b9762
David <d6797125@pm.me>
2021-12-14 15:16:23 +0100

    build: add Location model

--- TourGuide/src/test/java/tourGuide/TestRewardsService.java
+++ TourGuide/src/test/java/tourGuide/TestRewardsService.java
@@ -10,10 +10,8 @@ import org.junit.Test;
 
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import rewardCentral.RewardCentral;
 import tourGuide.config.InternalTestHelper;
 import tourGuide.proxy.GpsUtilProxy;
