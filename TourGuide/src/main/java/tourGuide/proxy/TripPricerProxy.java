fd727c79de9f8eb164ecb38bcce6242142f81878
David <d6797125@pm.me>
2021-12-15 11:59:00 +0100

    feat: add FeignClient to TripPricerProxy

--- TourGuide/src/main/java/tourGuide/proxy/TripPricerProxy.java
+++ TourGuide/src/main/java/tourGuide/proxy/TripPricerProxy.java
@@ -1,6 +1,21 @@
 package tourGuide.proxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tourGuide.model.Provider;
+
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "TripPricer", url = "localhost:7933")
 public interface TripPricerProxy {
 
-
+    @GetMapping(value="/getPrice")
+    List<Provider> getPrice(@RequestParam String key,
+                                   @RequestParam ("attractionUuid") UUID attractionUuid,
+                                   @RequestParam ("adultsCount") int adultsCount,
+                                   @RequestParam ("childrenCount") int childrenCount,
+                                   @RequestParam ("nightsNumber") int nightsNumber,
+                                   @RequestParam ("rewardsPoints") int rewardsPoints);
 }
