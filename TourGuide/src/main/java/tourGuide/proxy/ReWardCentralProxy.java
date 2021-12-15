d5baa74c33e18e3e6dcd732df76fae5f767b211c
David <d6797125@pm.me>
2021-12-15 10:23:49 +0100

    feat: add FeignClient to RewardCentralProx

--- TourGuide/src/main/java/tourGuide/proxy/ReWardCentralProxy.java
+++ TourGuide/src/main/java/tourGuide/proxy/ReWardCentralProxy.java
@@ -1,5 +1,8 @@
 package tourGuide.proxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "RewardCentral", url = "localhost:7922")
 public interface ReWardCentralProxy {
 
 
