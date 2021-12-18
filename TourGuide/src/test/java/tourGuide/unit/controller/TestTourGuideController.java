b224ad7c7874ddf3d91f3b3d07709db4e790d6ec
David <d6797125@pm.me>
2021-12-18 18:31:21 +0100

    test(controller): add shouldAccessHomePage endpoint test

--- TourGuide/src/test/java/tourGuide/unit/controller/TestTourGuideController.java
+++ TourGuide/src/test/java/tourGuide/unit/controller/TestTourGuideController.java
@@ -1,13 +1,31 @@
 package tourGuide.unit.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class TestTourGuideController {
 
+    @Autowired
+    private MockMvc mock;
 
+    @Test
+    public void shouldAccessHomePage() throws Exception {
+
+        mock.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Greetings from TourGuide!")));
+    }
 }
